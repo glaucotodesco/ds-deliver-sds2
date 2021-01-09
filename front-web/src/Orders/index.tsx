c4b1a66ae7fbeb5d530a71684e648878ab9bd39e
glauco.todesco <glauco.todesco@facens.br>
2021-01-08 23:51:40 -0300

    Product List and Product Card

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
+import StepHeaders from './StepsHeader';
+import './styles.css';
+import { Product } from './types';
+
+function Orders() {
+
+    const[products,setProducts]=useState<Product[]>([]);
+
+    useEffect( () => {
+        fetchProducts()
+        .then( response => setProducts(response.data))
+        .catch(error => console.log(error));
+    } , [] );
+
+    return (
+        <div className="orders-container">
+            <StepHeaders/>
+            <ProductsList products={products}/>
+        </div>
+    )
+
+}
+
+export default Orders;
