74692d56f0198922dfdab43cb7a4fffc53b02dec
glauco.todesco <glauco.todesco@facens.br>
2021-01-09 18:22:19 -0300

    Order Location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepHeaders from './StepsHeader';
 import './styles.css';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 
 function Orders() {
 
     const[products,setProducts]=useState<Product[]>([]);
+
+    const[orderLocation,setOrderLocation]=useState<OrderLocationData>();
 
     useEffect( () => {
         fetchProducts()
@@ -19,6 +22,7 @@ function Orders() {
         <div className="orders-container">
             <StepHeaders/>
             <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 
